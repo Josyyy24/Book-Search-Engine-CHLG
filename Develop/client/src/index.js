5c971c2d4b58f5d541b3a468887ddb3ca2a1e2d1
Joselyn Nuno <nunojosy0420@gmail.com>
2024-08-21 03:37:44 -0500

    added index.js folder

---
+++ Develop/client/src/index.js
@@ -0,0 +1,32 @@
+// Remove the import statement for 'React'
+import ReactDOM from 'react-dom';
+import { ApolloProvider, InMemoryCache, ApolloClient, createHttpLink } from '@apollo/client';
+import App from './App';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+    });
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+);
+
