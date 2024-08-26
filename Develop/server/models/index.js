8199b6a709c00440e3a6f97d650e04f5a1969e45
Joselyn Nuno <nunojosy0420@gmail.com>
2024-08-25 19:21:10 -0500

    edited index to export typeDefs and resolvers

--- Develop/server/models/index.js
+++ Develop/server/models/index.js
@@ -1,3 +1,7 @@
 const User = require('./User');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
 
 module.exports = { User };
+
+module.exports = { User, typeDefs, resolvers };
